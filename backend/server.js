20b7ecb0063ff7562922a9b725602145ac6a6bbf
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-02-17 12:59:41 +0000

    initial route established

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const PORT = process.env.PORT || 3000;
+const URI = process.env.URI;
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
