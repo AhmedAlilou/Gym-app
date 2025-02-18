53f5987d81e5fb86f191a175281323138e2d9052
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-02-18 15:47:16 +0000

    added timestamps on schemas

--- backend/models/exercise.js
+++ backend/models/exercise.js
@@ -2,21 +2,24 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const exerciseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    reps: {
+      type: Number,
+      required: true
+    },
+    sets: {
+      type: Number,
+      required: true
+    },
+    weight: {
+      type: Number,
+      required: true
+    }
   },
-  reps: {
-    type: Number,
-    required: true
-  },
-  sets: {
-    type: Number,
-    required: true
-  },
-  weight: {
-    type: Number,
-    required: true
-  }
-});
+  { timestamps: true }
+);
--- backend/models/workout.js
+++ backend/models/workout.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    exercises: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+      }
+    ]
   },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
-    }
-  ]
-});
+  { timestamps: true }
+);
