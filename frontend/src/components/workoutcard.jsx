eed709a5a065fb093b780956b168db0338374652
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-02-22 09:35:09 +0000

    exercises also added to display of workouts

--- frontend/src/components/workoutcard.jsx
+++ frontend/src/components/workoutcard.jsx
@@ -3,8 +3,23 @@ import React from "react";
 function Workoutcard(props) {
   return (
     <div className="workout text-white bg-[#333] p-4 rounded w-[90vw] mb-[2vh] ml-[2vw]">
-      <h2>{props.title}</h2>
+      <h2 className="font-bold text-[1.6rem]">{props.title}</h2>
       <p>{props.description}</p>
+      <div className="exercises">
+        {props.exercises && props.exercises.length > 0 ? (
+          props.exercises.map((exercise) => (
+            <div key={exercise._id} className="exercise">
+              <h2>{exercise.name}</h2>
+              <p>{exercise.description}</p>
+              <p>{exercise.sets} sets</p>
+              <p>{exercise.reps} reps</p>
+              <p>{exercise.weight} kg</p>
+            </div>
+          ))
+        ) : (
+          <p>No exercises</p>
+        )}
+      </div>
       <p>{props.id}</p>
     </div>
   );
--- frontend/src/components/workoutcards.jsx
+++ frontend/src/components/workoutcards.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Workoutcard from "./workoutcard";
 
 function Workoutcards() {
   const [workouts, setWorkouts] = useState([]);
+
   useEffect(() => {
-    fetch("http://localhost:3000/workouts").then((res) =>
-      res.json().then((data) => {
+    fetch("http://localhost:3000/workouts")
+      .then((res) => res.json())
+      .then((data) => {
         setWorkouts(data);
-      })
-    );
+      });
   }, []);
+
+  useEffect(() => {
+    workouts.forEach((workout) => console.log(workout.exercises));
+  }, [workouts]);
+
   return (
     <div className="workouts">
       {workouts.map((workout) => (
@@ -18,6 +23,7 @@ function Workoutcards() {
           <Workoutcard
             title={workout.title}
             description={workout.description}
+            exercises={workout.exercises}
             id={workout._id}
           />
         </div>
