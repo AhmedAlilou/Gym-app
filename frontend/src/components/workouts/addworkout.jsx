a8f4dc48c614f4882d549c6d1a3473f6cd1e2bab
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-02-27 18:13:26 +0000

    add new workout button

---
+++ frontend/src/components/workouts/addworkout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { IoMdAddCircle } from "react-icons/io";
+
+function Addworkout() {
+  return (
+    <button className="addworkout">
+      <IoMdAddCircle className="h-[90%] w-[70%] m-auto" />
+    </button>
+  );
+}
+// font-size: 2.3rem;
+// font-weight: 700;
+// color: #fff;
+// margin-top: 2vh;
+// margin-left: 2vw;
+
+export default Addworkout;
--- frontend/src/pages/workouts.jsx
+++ frontend/src/pages/workouts.jsx
@@ -3,13 +3,18 @@ import React from "react";
 
 // components
 import Navbar from "../components/navbar";
+import Addworkout from "../components/workouts/addworkout.jsx";
 import Workoutcards from "../components/workouts/workoutcards.jsx";
 
 function Workouts() {
   return (
     <>
       <Navbar />
-      <div className="title">Your Workouts</div>
+      <div className="flex  items-center mb-[2vh]">
+        <div className="title">Your Workouts</div>
+        <Addworkout />
+      </div>
+
       <Workoutcards />
     </>
   );
