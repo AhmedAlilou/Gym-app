96b49b3296c533b4700485dd8dc6f1e10e9be413
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-03-08 12:05:11 +0000

    made workouts a dropdown

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,7 +3,7 @@ import Navbarbutton from "./navbarbutton";
 
 function Navbar() {
   return (
-    <div className="w-[100vw] h-[6vh] bg-[#252525] flex justify-around text-white">
+    <div className="sticky top-0 w-[100vw] h-[6vh] bg-[#252525] flex justify-around text-white ">
       <Navbarbutton to="/" name="Home" />
       <Navbarbutton to="/workouts" name="Workouts" />
       <Navbarbutton to="/startworkout" name="Start workout" />
--- frontend/src/components/workouts/workoutcard.jsx
+++ frontend/src/components/workouts/workoutcard.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Workoutsidebar from "./workoutsidebar";
 import Exercises from "./exercises";
+import { FaChevronDown } from "react-icons/fa";
 
 function Workoutcard(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
   const workout = props.workout;
+
   return (
     <div className="workout text-white bg-[#333] p-4 rounded-3xl w-[90vw] mb-[2vh] ml-[2vw]">
-      <h2 className="font-bold text-[1.6rem]">{workout.title}</h2>
-      <p>{workout.description}</p>
-      <div className="flex flex-row ">
-        <Exercises workout={workout} />
-        <Workoutsidebar workout={workout} />
+      <div
+        className="flex justify-between items-center cursor-pointer"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <div>
+          <h2 className="font-bold text-[1.6rem]">{workout.title}</h2>
+          <p>{workout.description}</p>
+        </div>
+        <FaChevronDown
+          className={`transition-transform duration-300 ${
+            isExpanded ? "rotate-180" : ""
+          }`}
+        />
       </div>
+
+      {isExpanded && (
+        <div className="flex flex-row mt-4">
+          <Exercises workout={workout} />
+          <Workoutsidebar workout={workout} />
+        </div>
+      )}
     </div>
   );
 }
