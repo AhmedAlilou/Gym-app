d208ab4a17f992dcd436d664b9497da6720d385b
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-03-10 20:14:28 +0000

    added styles to confirmation modal

--- frontend/src/components/confirmation/deletemodal.jsx
+++ frontend/src/components/confirmation/deletemodal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useWorkoutStore } from "../../store/workoutStore";
+import "./styles/deletemodal.css";
 
 function Deletemodal() {
   const currentWorkoutID = useWorkoutStore((state) => state.currentWorkoutID);
@@ -9,21 +10,32 @@ function Deletemodal() {
   const confirmDeleteModal = useWorkoutStore(
     (state) => state.confirmDeleteModal
   );
+
   const handleConfirm = () => {
     useWorkoutStore.getState().deleteWorkout(currentWorkoutID);
     useWorkoutStore.getState().setConfirmDeleteModal(false);
   };
+
   const handleCancel = () => {
     useWorkoutStore.getState().setConfirmDeleteModal(false);
   };
+
   return confirmDeleteModal ? (
     <div className="modal">
-      <button className="bg-white" onClick={handleConfirm}>
-        CONFIRM
-      </button>
-      <button className="bg-white" onClick={handleCancel}>
-        CANCEL
-      </button>
+      <div className="modal-box">
+        <h2 className="text-xl font-bold mb-4 text-white">Confirm Delete</h2>
+        <p className="text-gray-300 mb-4">
+          Are you sure you want to delete this workout?
+        </p>
+        <div className="modal-buttons">
+          <button className="cancel-button" onClick={handleCancel}>
+            Cancel
+          </button>
+          <button className="confirm-button" onClick={handleConfirm}>
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
   ) : null;
 }
