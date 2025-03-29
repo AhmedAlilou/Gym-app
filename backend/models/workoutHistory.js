1b7ec3e23b5654500ffc36e4713de7200b609ce6
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-03-29 09:36:09 +0000

    intitialising new workoutHistory collection

---
+++ backend/models/workoutHistory.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutHistorySchema = new Schema(
+  {
+    workoutId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workout",
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    timeStart: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => {
+          return /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid time format! Use HH:MM (24-hour format)`
+      }
+    },
+    timeEnd: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => {
+          return /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid time format! Use HH:MM (24-hour format)`
+      }
+    },
+    exercises: [
+      {
+        name: { type: String, required: true },
+        sets: [
+          { reps: { type: Number, required: true }, weight: { type: Number } }
+        ]
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+const WorkoutHistory = mongoose.model("WorkoutHistory", workoutHistorySchema);
+module.exports = WorkoutHistory;
