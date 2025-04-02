0f64ad3616d8dc5e7ecf67353dfd64a5145f9251
AhmedAlilou <90469468+AhmedAlilou@users.noreply.github.com>
2025-04-02 16:50:04 +0100

    listed workout properties and added start workout button

--- frontend/src/components/startWorkout/currentworkout.jsx
+++ frontend/src/components/startWorkout/currentworkout.jsx
@@ -1,5 +1,6 @@
 import { React, use, useEffect } from "react";
 import { useWorkoutHistoryStore } from "../../store/workoutHistoryStore.js";
+import Start from "./start.jsx";
 
 function Currentworkout() {
   const currentWorkout = useWorkoutHistoryStore(
@@ -21,8 +22,27 @@ function Currentworkout() {
   return (
     <div className="flex-1 bg-[#1f1f1f] rounded-lg p-4">
       {JSON.stringify(currentWorkout).length > 5 ? ( // tacky way to check if object is empty
-        <div className="text-white">
-          <h2 className="text-2xl font-bold mb-4">{currentWorkout.title}</h2>
+        <div className="flex flex-col h-full">
+          <div className="text-white flex flex-row justify-between items-center">
+            <div className="workout-info">
+              <h2 className="text-2xl font-bold mb-4">
+                {currentWorkout.title}
+              </h2>
+              <p className="text-lg mb-2">{currentWorkout.description}</p>
+            </div>
+
+            <Start />
+          </div>
+          <div>
+            <h3 className="text-white text-lg font-semibold mt-4">
+              Exercises:
+            </h3>
+            {currentWorkout.exercises.map((exercise, index) => (
+              <li key={index} className="text-white mb-2">
+                {exercise.name}
+              </li>
+            ))}
+          </div>
         </div>
       ) : (
         <div className="flex items-center justify-center h-full">
---
+++ frontend/src/components/startWorkout/start.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Start() {
+  return (
+    <button className="bg-[#3b2a75]  hover:bg-[#543da2] w-[10vw] h-[5vh] m-[2%]">
+      {" "}
+      START{" "}
+    </button>
+  );
+}
+
+export default Start;
